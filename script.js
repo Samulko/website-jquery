f70fc7a3541c21399d0b3fa06dcd7e8ae5dd79d3
Samuel Slezak <sml.slezak@gmail.com>
2023-03-17 19:16:18 +0100

    Seventh commit

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 $(document).ready(function() {
-  // Redirect to "work.html" when root URL is requested
-  if (window.location.pathname === '/') {
-    window.location.href = 'work.html';
-  }
-  
+  // Load the navigation bar into the <header> tag
+  $('header').load('header.html', function() {
+    // Add click event listeners to navigation links
+    $("nav ul li a").click(function() {
+      // Remove "active" class from all navigation links
+      $("nav ul li").removeClass("active");
+      // Add "active" class to clicked navigation link
+      $(this).parent().addClass("active");
+      // Load content for clicked link using AJAX
+      var page = $(this).attr("href");
+      if (page !== "#") {
+        $("main").load(page);
+      }
+      // Prevent default link behavior
+      return false;
+    });
+  });
+
   // Load content for "Work" link
   $("main").load("work.html");
-  
-  // Add click event listeners to navigation links
-  $("nav ul li a").click(function() {
-    // Remove "active" class from all navigation links
-    $("nav ul li").removeClass("active");
-    // Add "active" class to clicked navigation link
-    $(this).parent().addClass("active");
-    // Load content for clicked link using AJAX
-    var page = $(this).attr("href");
-    if (page !== "#") {
-      $("main").load(page);
-    }
-    // Prevent default link behavior
-    return false;
+});
+
+// Add click event listener for project thumbnail links
+$(document).on("click", ".col a", function(e) {
+  e.preventDefault();
+  var projectPage = $(this).attr("href");
+  $("main").load("project/project-layout.html", function() {
+    $("header").load("header.html");
+    $("main").load(projectPage);
+    // Update the page title dynamically
+    $.ajax({
+      url: projectPage,
+      dataType: "html",
+      success: function(data) {
+        var title = $(data).filter("title").text();
+        $("head title").text(title);
+      }
+    });
   });
 });
